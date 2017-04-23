f0f423a639da1d74d6d320ea2d4ffce206f11eae
Weilong Zhao <weilong@weilongs-macbook-air.local>
2017-04-23 23:51:51 +0800

    Initial Commit

---
+++ FansArea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FansArea
+//
+//  Created by Weilong Zhao on 23/4/17.
+//  Copyright © 2017 Weilong Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
