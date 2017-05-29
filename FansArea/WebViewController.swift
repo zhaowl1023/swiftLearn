9c1d946515dec28acafabebc8f79a273a93ed629
zhaoweilong <zhaoweilong1023@gmail.com>
2017-05-30 02:29:29 +0800

    20170529

---
+++ FansArea/WebViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WebViewController.swift
+//  FansArea
+//
+//  Created by Weilong Zhao on 30/5/17.
+//  Copyright © 2017 Weilong Zhao. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
